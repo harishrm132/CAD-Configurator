1a111a60a29f742615012cf0f033aee74659efd7
Harish <harishrm132@gmail.com>
2021-11-10 23:25:36 +0530

    Add project files.

---
+++ CAD Configurator/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using CAD_Configurator.ViewModels;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CAD_Configurator
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
